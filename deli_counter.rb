cb7f3cef377df4661cc467a844cfe52e565c20ca
your_name <your_email@example.com>
2020-10-29 18:24:46 -0400

    commit tree ecf126ce089237c9a7df2672f5bbe63bab9ccc21

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,37 @@
-# Write your code here.
+require 'pry'
+
+def line(katz_dali)
+    new_line= []
+
+    if katz_dali.empty?
+        puts "The line is currently empty."
+    else
+        number_in_line = 1
+        katz_dali.each do |people|
+        new_line << "#{number_in_line}. #{people}"
+        number_in_line += 1
+       
+    end 
+    puts "The line is currently: #{new_line.join(" ")}"
+end  
+    
+
+
+    def take_a_number(katz_dali, name)
+            katz_dali << name 
+            puts "Welcome, #{name}. You are number #{katz_dali.length} in line."
+        end 
+    end
+
+    def now_serving(katz_dali)
+        if katz_dali.empty?
+            puts "There is nobody waiting to be served!"
+        else 
+            puts "Currently serving #{katz_dali.shift}."
+        end
+
+    end 
+    
+    
+
+    
